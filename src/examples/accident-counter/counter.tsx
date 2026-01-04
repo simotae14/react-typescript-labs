1fe32498bc7980d2fba5651ba528bd3d2ae2ffdc
Simone Taeggi <startae14@gmail.com>
2026-01-04 17:28:31 +0100

    08 Typing useState

--- src/examples/accident-counter/counter.tsx
+++ src/examples/accident-counter/counter.tsx
@@ -1,15 +1,18 @@
+import React from 'react';
+
 import { Card } from '$/common/components/card';
 import { Button } from './button';
 
 export const Counter = () => {
+  const [count, setCount] = React.useState(0);
   return (
     <Card className="border-primary-500 flex w-2/3 flex-col items-center gap-8">
       <h1>Days Since the Last Accident</h1>
-      <p className="text-6xl">0</p>
+      <p className="text-6xl">{count}</p>
       <div className="flex gap-2">
-        <Button>➖ Decrement</Button>
-        <Button>🔁 Reset</Button>
-        <Button>➕ Increment</Button>
+        <button onClick={() => setCount((prev) => prev - 1)}>➖ Decrement</button>
+        <button onClick={() => setCount(0)}>🔁 Reset</button>
+        <button onClick={() => setCount(count + 1)}>➕ Increment</button>
       </div>
       <form className="flex items-center gap-2" onSubmit={(e) => e.preventDefault()}>
         <input
