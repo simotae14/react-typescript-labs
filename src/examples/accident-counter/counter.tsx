129a0dc006d7d94b08b7ce2802fd577eb1d13b0e
Simone Taeggi <startae14@gmail.com>
2026-01-05 12:10:50 +0100

    09 Typing useState exercise

--- src/examples/accident-counter/counter.tsx
+++ src/examples/accident-counter/counter.tsx
@@ -5,6 +5,12 @@ import { Button } from './button';
 
 export const Counter = () => {
   const [count, setCount] = React.useState(0);
+
+  const [draftCount, setDraftCount] = React.useState(0);
+
+  const handleSubmit: React.ChangeEventHandler<HTMLInputElement> = (e) =>
+    setDraftCount(e.target.valueAsNumber);
+
   return (
     <Card className="border-primary-500 flex w-2/3 flex-col items-center gap-8">
       <h1>Days Since the Last Accident</h1>
@@ -14,11 +20,18 @@ export const Counter = () => {
         <button onClick={() => setCount(0)}>🔁 Reset</button>
         <button onClick={() => setCount(count + 1)}>➕ Increment</button>
       </div>
-      <form className="flex items-center gap-2" onSubmit={(e) => e.preventDefault()}>
+      <form
+        className="flex items-center gap-2"
+        onSubmit={(e) => {
+          e.preventDefault();
+          setCount(draftCount);
+        }}
+      >
         <input
           className="ring-primary-600 focus:border-primary-800 rounded border border-slate-500 px-4 py-2 outline-none focus:ring-2"
           type="number"
-          value={0}
+          onChange={handleSubmit}
+          value={draftCount}
         />
         <Button>Update Counter</Button>
       </form>
