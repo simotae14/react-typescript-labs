03f8dc55656dc4a4230282a7bb970bafc06e6f5f
Simone Taeggi <startae14@gmail.com>
2026-01-05 13:15:57 +0100

    11 Typing Form Events exercise

--- src/examples/accident-counter/counter.tsx
+++ src/examples/accident-counter/counter.tsx
@@ -3,6 +3,29 @@ import React from 'react';
 import { Card } from '$/common/components/card';
 import { Button } from './button';
 
+type CounterFormProps = {
+  onSubmit: React.FormEventHandler<HTMLFormElement>;
+};
+
+const CounterForm = ({ onSubmit }: CounterFormProps) => {
+  const [draftCount, setDraftCount] = React.useState(0);
+
+  const handleSubmit: React.ChangeEventHandler<HTMLInputElement> = (e) =>
+    setDraftCount(e.target.valueAsNumber);
+  return (
+    <form className="flex items-center gap-2" onSubmit={onSubmit}>
+      <input
+        className="ring-primary-600 focus:border-primary-800 rounded border border-slate-500 px-4 py-2 outline-none focus:ring-2"
+        type="number"
+        onChange={handleSubmit}
+        value={draftCount}
+        name="count"
+      />
+      <Button>Update Counter</Button>
+    </form>
+  );
+};
+
 type CounterControlsProps = {
   count: number;
   setCount: React.Dispatch<React.SetStateAction<number>>;
@@ -21,31 +44,21 @@ const CounterControls = ({ count, setCount }: CounterControlsProps) => {
 export const Counter = () => {
   const [count, setCount] = React.useState(0);
 
-  const [draftCount, setDraftCount] = React.useState(0);
-
-  const handleSubmit: React.ChangeEventHandler<HTMLInputElement> = (e) =>
-    setDraftCount(e.target.valueAsNumber);
-
   return (
     <Card className="border-primary-500 flex w-2/3 flex-col items-center gap-8">
       <h1>Days Since the Last Accident</h1>
       <p className="text-6xl">{count}</p>
       <CounterControls count={count} setCount={setCount} />
-      <form
-        className="flex items-center gap-2"
+      <CounterForm
         onSubmit={(e) => {
           e.preventDefault();
-          setCount(draftCount);
+
+          const formData = new FormData(e.currentTarget);
+          const newCount = Number(formData.get('count'));
+
+          setCount(newCount);
         }}
-      >
-        <input
-          className="ring-primary-600 focus:border-primary-800 rounded border border-slate-500 px-4 py-2 outline-none focus:ring-2"
-          type="number"
-          onChange={handleSubmit}
-          value={draftCount}
-        />
-        <Button>Update Counter</Button>
-      </form>
+      />
     </Card>
   );
 };
